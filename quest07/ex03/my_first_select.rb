2943c71626c903bb237cbb5212e39bf4fc82a629
Chima Ekeneme <sebastinechima@gmail.com>
2020-09-09 15:02:03 +0100

    Select a specific field from csv file

---
+++ quest07/ex03/my_first_select.rb
@@ -0,0 +1,32 @@
+require 'csv'
+
+class MyFirstSelect
+  def initialize(filename_db)
+    @file = CSV.read(filename_db)
+    @column
+  end
+
+  def to_s
+    puts "Column Number: #{@column}"
+    return "File: #{@file}"
+  end
+  def where(column_name, value)
+    @file[0].each_with_index{|data, idx| @column=idx if column_name==data}
+    arr = []
+    head = @file[0]
+    @file[1..-1].map do
+      |item|
+      new_hash = Hash.new
+      if item.include? value
+        item.length.times{|num| new_hash[head[num]]=item[num]}
+        arr<<new_hash
+      end
+    end
+    print arr
+  end
+end
+
+# my_csv = MyFirstSelect.new("nba_player_data.csv")
+my_csv = MyFirstSelect.new("data.csv")
+# puts my_csv
+puts my_csv.where("name" , "Alaa Abdelnaby")
